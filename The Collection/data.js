4b51eb5b078dcc99a6d0759f878f8e26e51623fd
Feruzteame <teamefitwi@gmail.com>
2022-01-13 18:22:52 +0100

    add audio player, nav-bar and logo

--- The Collection/data.js
+++ The Collection/data.js
@@ -6,9 +6,39 @@
       time: 1994
     },
     {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
+        Title: "Time After Time",
+        name: 'Cyndi Lauper',
+        time: 2000
+      },
+      {
+        Title: "Love me Two Times",
+        name: 'The Doors',
+        time: 1980
+      },
+      {
+        Title: "Time is on my Side",
+        name: 'The Rolling Stones',
+        time: 1678
+      },
+      {
+        Title: "Time of my Life",
+        name: 'David Cook',
+        time: 1967
+      },
+      {
+        Title: "Any Time at all",
+        name: 'The Beatles',
+        time: 1995
+      },
+      {
+        Title: "Too Much Time on my Hands",
+        name: 'Styx',
+        time: 1895
+      },
+      {
+        Title: "Big Time",
+        name: 'Peter Gabriel',
+        time: 2005
       },
       {
         Title: "One Moment in Time",
@@ -16,9 +46,9 @@
         time: 1994
       },
       {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
+        Title: "One Time",
+        name: 'Justin Bieber',
+        time: 2010
       },
       {
         Title: "One Moment in Time",
@@ -26,30 +56,50 @@
         time: 1994
       },
       {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
-      },
-      {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
-      },
-      {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
-      },
-      {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
-      },
-      {
-        Title: "One Moment in Time",
-        name: 'Whitney Houston',
-        time: 1994
-      },
+          Title: "Time After Time",
+          name: 'Cyndi Lauper',
+          time: 2000
+        },
+        {
+          Title: "Love me Two Times",
+          name: 'The Doors',
+          time: 1980
+        },
+        {
+          Title: "Time is on my Side",
+          name: 'The Rolling Stones',
+          time: 1678
+        },
+        {
+          Title: "Time of my Life",
+          name: 'David Cook',
+          time: 1967
+        },
+        {
+          Title: "Any Time at all",
+          name: 'The Beatles',
+          time: 1995
+        },
+        {
+          Title: "Too Much Time on my Hands",
+          name: 'Styx',
+          time: 1895
+        },
+        {
+          Title: "Big Time",
+          name: 'Peter Gabriel',
+          time: 2005
+        },
+        {
+          Title: "One Moment in Time",
+          name: 'Whitney Houston',
+          time: 1994
+        },
+        {
+          Title: "One Time",
+          name: 'Justin Bieber',
+          time: 2010
+        },
   
     
   ]
