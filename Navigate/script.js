bbc615f7169d32f645712e20b2bc963fef726ed7
Feruzteame <teamefitwi@gmail.com>
2022-01-17 15:30:20 +0100

    change html file

--- Navigate/script.js
+++ Navigate/script.js
@@ -3,21 +3,20 @@
 const olList = document.getElementById('olList')
 console.log(olList.children.length);
 
-let firstChild = olList.firstChild
-console.log(firstChild)
-
-const  lastChild = document.createElement('LI');
-lastChild.innerText = "five";
-
+let firstChild = olList.children[0]
+const  lastChild = olList.children[4];
 olList.insertBefore(lastChild, firstChild)
 
 // move h2  section3 to section2 
-const secondSection = document.getElementById('secondSection')
-const sec3h2 = document.getElementById('sec3');
-secondSection.appendChild(sec3h2)
+const main = document.getElementById('main')
+const secondSection = main.children[2];
+console.log(secondSection);
+const thirdSection = main.children[2];
+const thirdSectionDiv = thirdSection.children[0].children[0]
+console.log(thirdSectionDiv);
+secondSection.appendChild(thirdSectionDiv)
 
 // remove section 3
-const thirdSection = document.getElementById('thirdSection');
 thirdSection.remove();
 
 
